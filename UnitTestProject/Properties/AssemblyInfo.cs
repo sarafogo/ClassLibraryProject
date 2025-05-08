ee79488af32306ae699261f3051b279d1b26e6a3
sara.f.silva.simao <sara.f.silva.simao@WINAPXHTDDWISEE>
2025-05-08 15:38:48 -0300

    primeiro commit

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avanade")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Avanade 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc96e122-3f84-4ea0-b799-134fbda343f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryProject;
+
+namespace ClassLibraryProject.Tests
+{
+    [TestClass]
+    public class Class1Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var class1 = new Class1();
+
+            // Act
+            // (Chame o método que você deseja testar aqui)
+
+            // Assert
+            Assert.IsNotNull(class1); // Exemplo de verificação
+        }
+    }
+}
